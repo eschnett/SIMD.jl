b2a42dfae2f85b96b2c785983b2ae50830baf80a
Eddie Groshev <eddie_g_89@hotmail.com>
2025-08-14 12:10:50 -0700

    re-add restriction

--- src/LLVM_intrinsics.jl
+++ src/LLVM_intrinsics.jl
@@ -807,6 +807,9 @@ end
 ###########
 
 @generated function bitmask(x::LVec{N, Bool}) where {N}
+    if N > 128 # Julia doesn't export anything larger than UInt128
+        return :(throw(ArgumentError(("vector length $(N) must be <= 128"))))
+    end
     P = nextpow(2, max(N, 8))
     T = Symbol("UInt", P)
     if N == P
