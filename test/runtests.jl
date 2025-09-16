d787c2bcc72858f1e13fe559125dee1156a9708c
Simeon David Schaub <simeon@schaub.rocks>
2025-09-16 12:38:48 +0200

    only run OpenCL tests on select platforms
    and fix some Windows issues with pocl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1019,6 +1019,10 @@ llvm_ir(f, args) = sprint(code_llvm, f, Base.typesof(args...))
         end
     end
 
-include("opencl.jl")
+if parse(Bool, get(ENV, "TEST_OPENCL", "true"))
+    include("opencl.jl")
+else
+    @info "Skipping OpenCL tests"
+end
 
 # end
